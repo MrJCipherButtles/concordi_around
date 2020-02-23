028d23c28b034b943b1cb2749bda15b66abe62a4
Divesh Patel <divesh.pat3l@gmail.com>
2020-02-22 21:50:09 -0500

    Added non-functioning search bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:concordi_around/positionedFloatingSearchBar.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
@@ -34,20 +35,30 @@ class MapSampleState extends State<MapSample> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: GoogleMap(
-        mapType: MapType.normal,
-        indoorViewEnabled: true,
-        initialCameraPosition: CameraPosition(target: LatLng(45.497593, -73.578487)), 
-        //CameraPosition(target: _initialPosition, zoom:18.5), 
-        //throws Failed assertion: line 22 of package google_maps_flutter/src/camera.dart
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
+      body: Stack(
+        children: <Widget>[
+          // Replace this container with your Map widget
+          Container(
+              child: GoogleMap(
+            mapType: MapType.normal,
+            indoorViewEnabled: true,
+            initialCameraPosition:
+                CameraPosition(target: LatLng(45.497593, -73.578487)),
+            //CameraPosition(target: _initialPosition, zoom:18.5),
+            //throws Failed assertion: line 22 of package google_maps_flutter/src/camera.dart
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+          )),
+          PositionedFloatingSearchBar()
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _goToCurrent,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
         tooltip: 'Get Location',
-        child: Icon(Icons.location_on),
+        child: Icon(Icons.my_location),
       ),
     );
   }
@@ -62,14 +73,14 @@ class MapSampleState extends State<MapSample> {
   Future<void> _goToCurrent() async {
     final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
 
-    var currentLocation = await geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+    var currentLocation = await geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.best);
 
     CameraPosition _currentPos = CameraPosition(
-      target: LatLng(currentLocation.latitude, currentLocation.longitude),
-      zoom: 18.5);  
+        target: LatLng(currentLocation.latitude, currentLocation.longitude),
+        zoom: 18.5);
 
     final GoogleMapController controller = await _controller.future;
     controller.animateCamera(CameraUpdate.newCameraPosition(_currentPos));
   }
-
 }
---
+++ lib/positionedFloatingSearchBar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class PositionedFloatingSearchBar extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: MediaQuery.of(context).padding.top + 5.0,
+      right: 15,
+      left: 15,
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20), color: Colors.white),
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              splashColor: Colors.grey,
+              icon: Icon(Icons.menu),
+              onPressed: () {},
+            ),
+            Expanded(
+              child: TextField(
+                cursorColor: Colors.black,
+                keyboardType: TextInputType.text,
+                textInputAction: TextInputAction.go,
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.symmetric(horizontal: 15),
+                    hintText: "Search..."),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: CircleAvatar(
+                backgroundColor: Color.fromRGBO(147, 35, 57, 1),
+                child: Text('CU'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
