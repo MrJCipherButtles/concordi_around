4fcb7b72c09fc30c8fa8cbd1c0c08eba2ed94350
Joshua Butler <butler.joshua355@gmail.com>
2020-02-24 07:19:41 -0500

    Class model for a path

---
+++ lib/models/Path.dart
@@ -0,0 +1,32 @@
+
+import 'Coordinate.dart';
+import 'Segment.dart';
+
+class Path {
+  final List<Segment> _segments = <Segment>[];
+
+  Path(List<Coordinate> coordinateList) {
+    assert (coordinateList != null && coordinateList.length > 1);
+    for(var i = 0; i < coordinateList.length - 1; i++) {
+      _segments.add(Segment(coordinateList[i], coordinateList[i+1]));
+    }
+  }
+
+  Set<Coordinate> getCoordinatesInOrder() {
+    var ls = <Coordinate>{};
+    ls.add(_segments[0].source);
+    for(var segment in _segments) {
+      ls.add(segment.destination);
+    }
+    return ls;
+  }
+
+  @override
+  String toString(){
+    var buffer = StringBuffer();
+    for(var coordinate in getCoordinatesInOrder()) {
+      buffer.write(coordinate.toString());
+    }
+    return buffer.toString();
+  }
+}
