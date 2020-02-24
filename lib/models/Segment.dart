af93fac97e6acb67351819eab0a999ff0d549473
Joshua Butler <butler.joshua355@gmail.com>
2020-02-24 07:15:03 -0500

    Added function to check disability friendliness of a segment

--- lib/models/Segment.dart
+++ lib/models/Segment.dart
@@ -1,4 +1,5 @@
 import 'Coordinate.dart';
+import 'PortalCoordinate.dart';
 
 class Segment {
 
@@ -11,6 +12,16 @@ class Segment {
 
   Coordinate get source => _source;
   Coordinate get destination => _destination;
+
+  bool isDisabilityFriendly(){
+    //A segment is only false when source and destination are both not disability friendly
+    //e.g. top floor to bottom floor returns false, but stairs to classroom on the same floor returns true
+    if (_source is PortalCoordinate && _destination is PortalCoordinate) {
+      return (_source as PortalCoordinate).isDisabilityFriendly || (_destination as PortalCoordinate).isDisabilityFriendly;
+    }
+    //else one coordinate must not be a portal; therefore, true by default
+    return true;
+  }
 
   @override
   String toString() => _source.toString() + '->' + _destination.toString();
