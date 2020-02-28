d6520fb184169e8c1f66c2cffd1aad128fbd8089
Umer <anwarumer97@gmail.com>
2020-02-27 23:19:14 -0500

    added all portal coordinates for the 9th floor

--- lib/models/building_coordinates.dart
+++ lib/models/building_coordinates.dart
@@ -13,26 +13,161 @@ class BuildingSingleton {
 
   BuildingSingleton._internal() {
 
-    PortalCoordinate a = PortalCoordinate(
-        45.49719, -73.57933, '9', 'Hall', 'SGW',
+    PortalCoordinate j9F1 = PortalCoordinate(
+        45.497222, -73.579358, '9', 'Hall', 'SGW',
         adjCoordinates: <Coordinate>{});
 
-    PortalCoordinate b = PortalCoordinate(
-        45.49734, -73.57918, '9', 'Hall', 'SGW',
+    PortalCoordinate j9F2 = PortalCoordinate(
+        45.497282, -73.579296, '9', 'Hall', 'SGW',
         adjCoordinates: <Coordinate>{});
 
-    RoomCoordinate start = RoomCoordinate(
-        45.49713, -73.57919, '9', 'Hall', 'SGW',
+    PortalCoordinate j9F3 = PortalCoordinate(
+        45.497279, -73.579218, '9', 'Hall', 'SGW',
         adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F4 = PortalCoordinate(
+        45.497151, -73.579210, '9', 'Hall', 'SGW',
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F5 = PortalCoordinate(
+        45.497247, -73.579120, '9', 'Hall', 'SGW',
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F6 = PortalCoordinate(
+        45.497305, -73.579054, '9', 'Hall', 'SGW',
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F7 = PortalCoordinate(
+        45.497353, -73.579043, '9', 'Hall', 'SGW',
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F8 = PortalCoordinate(
+        45.497390, -73.579003, '9', 'Hall', 'SGW',
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F9 = PortalCoordinate(
+        45.497422, -73.579075, '9', 'Hall', 'SGW',
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F10 = PortalCoordinate(
+        45.497456, -73.579144, '9', 'Hall', 'SGW',
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F11 = PortalCoordinate(
+        45.497488, -73.579169, '9', 'Hall', 'SGW',
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F12 = PortalCoordinate(
+        45.497650, -73.579018, '9', 'Hall', 'SGW',
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F13 = PortalCoordinate(
+        45.497562, -73.578834, '9', 'Hall', 'SGW',
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F14 = PortalCoordinate(
+        45.497503, -73.578712, '9', 'Hall', 'SGW',
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F15 = PortalCoordinate(
+        45.497111, -73.57118, '9', 'Hall', 'SGW',
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F16 = PortalCoordinate(
+        45.497042, -73.579145, '9', 'Hall', 'SGW',
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F17 = PortalCoordinate(
+        45.496957, -73.578869, '9', 'Hall', 'SGW',
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F18 = PortalCoordinate(
+        45.496972, -73.578892, '9', 'Hall', 'SGW',
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F19 = PortalCoordinate(
+        45.497154, -73.578731, '9', 'Hall', 'SGW',
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F20 = PortalCoordinate(
+        45.497352, -73.578536, '9', 'Hall', 'SGW',
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F21 = PortalCoordinate(
+        45.497334, -73.578493, '9', 'Hall', 'SGW',
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F22 = PortalCoordinate(
+        45.497424, -73.578703, '9', 'Hall', 'SGW',
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F23 = PortalCoordinate(
+        45.497409, -73.578684, '9', 'Hall', 'SGW',
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F24 = PortalCoordinate(
+        45.497246, -73.578840, '9', 'Hall', 'SGW', type: "ESCALATOR-DOWN",
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F25 = PortalCoordinate(
+        45.497218, -73.578863, '9', 'Hall', 'SGW',
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F26 = PortalCoordinate(
+        45.497289, -73.579012, '9', 'Hall', 'SGW',
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F27 = PortalCoordinate(
+        45.497300, -73.579003, '9', 'Hall', 'SGW', type: "ESCALATOR-UP",
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F28 = PortalCoordinate(
+        45.497191, -73.579165, '9', 'Hall', 'SGW', type: "STAIRS",
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F29 = PortalCoordinate(
+        45.497047, -73.578826, '9', 'Hall', 'SGW', type: "STAIRS",
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F30 = PortalCoordinate(
+        45.497442, -73.578995, '9', 'Hall', 'SGW', type: "STAIRS",
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F31 = PortalCoordinate(
+        45.497408, -73.579041, '9', 'Hall', 'SGW', type: "ELEVATOR", isDisabilityFriendly: true,
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F32 = PortalCoordinate(
+        45.497084, -73.579120, '9', 'Hall', 'SGW',
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F33 = PortalCoordinate(
+        45.497077, -73.579111, '9', 'Hall', 'SGW',
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F34 = PortalCoordinate(
+        45.497052, -73.579032, '9', 'Hall', 'SGW',
+        adjCoordinates: <Coordinate>{});
+
+    PortalCoordinate j9F35 = PortalCoordinate(
+        45.497207, -73.578683, '9', 'Hall', 'SGW',
+        adjCoordinates: <Coordinate>{});
+
+
+    PortalCoordinate j9F36 = PortalCoordinate(
+        45.497319, -73.578772, '9', 'Hall', 'SGW', type: "ELEVATOR", isDisabilityFriendly: true,
+        adjCoordinates: <Coordinate>{});
+
+
+    PortalCoordinate j9F37 = PortalCoordinate(
+        45.497364, -73.578729, '9', 'Hall', 'SGW', type: "STAIRS",
+        adjCoordinates: <Coordinate>{});
+
 
     RoomCoordinate end = RoomCoordinate(45.49749, -73.57905, '8', 'Hall', 'SGW',
         adjCoordinates: <Coordinate>{});
 
-    a.addAdjCoordinate(start);
-    a.addAdjCoordinate(b);
-    b.addAdjCoordinate(end);
-
-    Floor eigth_floor = Floor('8', coordinates: {a, b, start, end});
+    // Floor eigth_floor = Floor('8', coordinates: {a, b, start, end});
 
     _building = Building('Hall', floors: {'8': eigth_floor});
   }
