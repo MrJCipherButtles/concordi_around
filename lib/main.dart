852067bf2302e4103d3710afea009605b06a70d9
Divesh Patel <divesh.pat3l@gmail.com>
2020-02-27 21:39:17 -0500

    ref #10 US-8: Create page for go button

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:concordi_around/mapNotifier.dart';
+import 'package:concordi_around/widgets/GoToPage.dart';
 import 'package:concordi_around/widgets/generalUI/positionedFloatingSearchBar.dart';
 import 'package:concordi_around/widgets/generalUI/sidebarDrawer.dart';
 import 'package:concordi_around/widgets/mapUI/floorSelectorAndEnterBuilding.dart';
@@ -39,17 +40,18 @@ class MapSampleState extends State<MapSample> {
   void initState() {
     super.initState();
     _geolocator = Geolocator()..forceAndroidLocationManager;
-    LocationOptions locationOptions = LocationOptions(accuracy: LocationAccuracy.best, distanceFilter: 1);
+    LocationOptions locationOptions =
+        LocationOptions(accuracy: LocationAccuracy.best, distanceFilter: 1);
     updateLocation();
-    _positionStream = _geolocator.getPositionStream(locationOptions).listen(
-        (Position pos) {
-          setState(() {
-            _position = pos;
-            _cameraPosition = CameraPosition(
-                target: LatLng(_position.latitude, _position.longitude),
-                zoom: 19.03);
-          });
-        });
+    _positionStream =
+        _geolocator.getPositionStream(locationOptions).listen((Position pos) {
+      setState(() {
+        _position = pos;
+        _cameraPosition = CameraPosition(
+            target: LatLng(_position.latitude, _position.longitude),
+            zoom: 19.03);
+      });
+    });
   }
 
   @override
@@ -63,7 +65,8 @@ class MapSampleState extends State<MapSample> {
   void updateLocation() async {
     try {
       final GoogleMapController controller = await _controller.future;
-      Position position = await _geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
+      Position position = await _geolocator
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
           .timeout(new Duration(seconds: 5));
       setState(() {
         _position = position;
@@ -79,13 +82,10 @@ class MapSampleState extends State<MapSample> {
 
   @override
   Widget build(BuildContext context) {
-
     MapNotifier mapNotifier = Provider.of<MapNotifier>(context);
 
-    if(_cameraPosition == null) {
-      _cameraPosition = CameraPosition(
-          target: LatLng(0, 0)
-      );
+    if (_cameraPosition == null) {
+      _cameraPosition = CameraPosition(target: LatLng(0, 0));
     }
 
     return MaterialApp(
@@ -111,147 +111,144 @@ class MapSampleState extends State<MapSample> {
                   onCameraMove: (CameraPosition cameraPosition) {
                     if (IsWithinHallStrictBound(cameraPosition.target) &&
                         cameraPosition.zoom > 18) {
-                        mapNotifier.setFloorPlanVisibility(true);
+                      mapNotifier.setFloorPlanVisibility(true);
                     } else {
                       mapNotifier.setFloorPlanVisibility(false);
                     }
-                    if(IsWithinHall(cameraPosition.target)) {
+                    if (IsWithinHall(cameraPosition.target)) {
                       mapNotifier.setEnterBuildingVisibility(true);
-                    }
-                    else {
+                    } else {
                       mapNotifier.setEnterBuildingVisibility(false);
                     }
                   },
                 )),
-                SearchBar(name: (String building) => {_goToSelectedBuilding("$building")}),
+                SearchBar(
+                    name: (String building) =>
+                        {_goToSelectedBuilding("$building")}),
                 SVGFloorPlans(),
-                                FloorSelectorAndEnterBuilding(
-                                      selectedFloor: (int floor) => {
-                                          mapNotifier.setSelectedFloor(floor)},
-                                      enterBuildingPressed: () => _goToHall8th(),
-                                    ),
-                              ],
-                            ),
-                            drawer: SidebarDrawer(),
-                            resizeToAvoidBottomInset: false,
-                            floatingActionButton: Column(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: <Widget>[
-                                  FloatingActionButton(
-                                    heroTag: 'unique1',
-                                    onPressed: _goToCurrent,
-                                    backgroundColor: Colors.white,
-                                    foregroundColor: Color.fromRGBO(147, 0, 47, 1),
-                                    tooltip: 'Get Location',
-                                    child: Icon(Icons.my_location),
-                                  ),
-                                  SizedBox(height: 16,),
-                                  FloatingActionButton(
-                                    heroTag: 'unique2',
-                                    onPressed: () {},
-                                    backgroundColor: Color.fromRGBO(147, 0, 47, 1),
-                                    foregroundColor: Colors.white,
-                                    child: Icon(Icons.directions),
-                                  ),
-                                ])
-                                ));
-                  }
-                
-                  Future<void> _goToCurrent() async {
-                    final GoogleMapController controller = await _controller.future;
-                    _cameraPosition = CameraPosition(
-                        target: LatLng(_position.latitude, _position.longitude),
-                        zoom: 19.03);
-                    controller.animateCamera(CameraUpdate.newCameraPosition(_cameraPosition));
-                  }
-                
-                  bool IsWithinHallStrictBound(LatLng latLng) {
-                    
-                    List<LatLng> coordsList = [
-                      LatLng(45.49726709926478, -73.57894677668811)];
-                
-                    return boundsFromLatLngList(coordsList)
-                        .contains(latLng);
-                  }
+                FloorSelectorAndEnterBuilding(
+                  selectedFloor: (int floor) =>
+                      {mapNotifier.setSelectedFloor(floor)},
+                  enterBuildingPressed: () => _goToHall8th(),
+                ),
+              ],
+            ),
+            drawer: SidebarDrawer(),
+            resizeToAvoidBottomInset: false,
+            floatingActionButton: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  FloatingActionButton(
+                    heroTag: 'unique1',
+                    onPressed: _goToCurrent,
+                    backgroundColor: Colors.white,
+                    foregroundColor: Color.fromRGBO(147, 0, 47, 1),
+                    tooltip: 'Get Location',
+                    child: Icon(Icons.my_location),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  FloatingActionButton(
+                    heroTag: 'unique2',
+                    onPressed: () {
+                      print("TESTTTTTTTTTTTTTTTTT");
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => GoToPage(),
+                        ),
+                      );
+                    },
+                    backgroundColor: Color.fromRGBO(147, 0, 47, 1),
+                    foregroundColor: Colors.white,
+                    child: Icon(Icons.directions),
+                  ),
+                ])));
+  }
 
-                  bool IsWithinHall(LatLng latLng) {
-                    
-                    List<LatLng> coordsList = [
-                      LatLng(45.49718, -73.57968),
-                      LatLng(45.49675, -73.57878),
-                      LatLng(45.49741, -73.57819),
-                      LatLng(45.49781, -73.57906)];
-                
-                    return boundsFromLatLngList(coordsList)
-                        .contains(latLng);
-                  }
-                
-                   LatLngBounds boundsFromLatLngList(List<LatLng> list) {
-                    assert(list.isNotEmpty);
-                    double x0, x1, y0, y1;
-                    for (LatLng latLng in list) {
-                      if (x0 == null) {
-                        x0 = x1 = latLng.latitude;
-                        y0 = y1 = latLng.longitude;
-                      } else {
-                        if (latLng.latitude > x1) x1 = latLng.latitude;
-                        if (latLng.latitude < x0) x0 = latLng.latitude;
-                        if (latLng.longitude > y1) y1 = latLng.longitude;
-                        if (latLng.longitude < y0) y0 = latLng.longitude;
-                      }
-                    }
-                    return LatLngBounds(northeast: LatLng(x1, y1), southwest: LatLng(x0, y0));
-                  }
-                
-                  Future<void> _goToSelectedBuilding(String name) async {
-                    final GoogleMapController controller = await _controller.future;
-                    if (name.contains("Hall")) {
-                      CameraPosition _currentPos = CameraPosition(
-                          target: LatLng(45.49726, -73.57895),
-                          zoom: 18.5);
-                      _goToHall8th();
-                    }
-                    else if (name.contains("Video")) {
-                      CameraPosition _currentPos = CameraPosition(
-                          target: LatLng(45.49683, -73.57793),
-                          zoom: 18.5);
-                      controller.animateCamera(CameraUpdate.newCameraPosition(_currentPos));
-                    }
-                    else if (name.contains("John")) {
-                      CameraPosition _currentPos = CameraPosition(
-                          target: LatLng(45.49531, -73.57901),
-                          zoom: 18.5);
-                      controller.animateCamera(CameraUpdate.newCameraPosition(_currentPos));
-                    }
-                    else if (name.contains("H806")) {
-                      CameraPosition _currentPos = CameraPosition(
-                          target: LatLng(45.49715, -73.57878),
-                          zoom: 21);
-                       _goToHall8th();
-                    }
-                    else if (name.contains("H832")) {
-                      CameraPosition _currentPos = CameraPosition(
-                          target: LatLng(45.49728, -73.57924),
-                          zoom: 21);
-                       _goToHall8th();
-                    }
-                    else if (name.contains("H860")) {
-                      CameraPosition _currentPos = CameraPosition(
-                          target: LatLng(45.49744, -73.57875),
-                          zoom: 21);
-                       _goToHall8th();
-                    }
-                  }
-                
-                  Future<void> _goToHall8th() async {
-                
-                    CameraPosition _currentPos = CameraPosition(bearing: 123.31752014160156, target: LatLng(45.49726709926478, -73.57894677668811), tilt: 0.0, zoom: 19.03557586669922);
-                
-                    final GoogleMapController controller = await _controller.future;
-                    controller.animateCamera(CameraUpdate.newLatLng(LatLng(45.49726709926478, -73.57894677668811)));
-                    controller.moveCamera(CameraUpdate.newLatLng(LatLng(45.49726709926478, -73.57894677668811)));
-                    controller.animateCamera(CameraUpdate.newCameraPosition(_currentPos));
-                
-                  }
-                }
+  Future<void> _goToCurrent() async {
+    final GoogleMapController controller = await _controller.future;
+    _cameraPosition = CameraPosition(
+        target: LatLng(_position.latitude, _position.longitude), zoom: 19.03);
+    controller.animateCamera(CameraUpdate.newCameraPosition(_cameraPosition));
+  }
+
+  bool IsWithinHallStrictBound(LatLng latLng) {
+    List<LatLng> coordsList = [LatLng(45.49726709926478, -73.57894677668811)];
+
+    return boundsFromLatLngList(coordsList).contains(latLng);
+  }
+
+  bool IsWithinHall(LatLng latLng) {
+    List<LatLng> coordsList = [
+      LatLng(45.49718, -73.57968),
+      LatLng(45.49675, -73.57878),
+      LatLng(45.49741, -73.57819),
+      LatLng(45.49781, -73.57906)
+    ];
+
+    return boundsFromLatLngList(coordsList).contains(latLng);
+  }
+
+  LatLngBounds boundsFromLatLngList(List<LatLng> list) {
+    assert(list.isNotEmpty);
+    double x0, x1, y0, y1;
+    for (LatLng latLng in list) {
+      if (x0 == null) {
+        x0 = x1 = latLng.latitude;
+        y0 = y1 = latLng.longitude;
+      } else {
+        if (latLng.latitude > x1) x1 = latLng.latitude;
+        if (latLng.latitude < x0) x0 = latLng.latitude;
+        if (latLng.longitude > y1) y1 = latLng.longitude;
+        if (latLng.longitude < y0) y0 = latLng.longitude;
+      }
+    }
+    return LatLngBounds(northeast: LatLng(x1, y1), southwest: LatLng(x0, y0));
+  }
+
+  Future<void> _goToSelectedBuilding(String name) async {
+    final GoogleMapController controller = await _controller.future;
+    if (name.contains("Hall")) {
+      CameraPosition _currentPos =
+          CameraPosition(target: LatLng(45.49726, -73.57895), zoom: 18.5);
+      _goToHall8th();
+    } else if (name.contains("Video")) {
+      CameraPosition _currentPos =
+          CameraPosition(target: LatLng(45.49683, -73.57793), zoom: 18.5);
+      controller.animateCamera(CameraUpdate.newCameraPosition(_currentPos));
+    } else if (name.contains("John")) {
+      CameraPosition _currentPos =
+          CameraPosition(target: LatLng(45.49531, -73.57901), zoom: 18.5);
+      controller.animateCamera(CameraUpdate.newCameraPosition(_currentPos));
+    } else if (name.contains("H806")) {
+      CameraPosition _currentPos =
+          CameraPosition(target: LatLng(45.49715, -73.57878), zoom: 21);
+      _goToHall8th();
+    } else if (name.contains("H832")) {
+      CameraPosition _currentPos =
+          CameraPosition(target: LatLng(45.49728, -73.57924), zoom: 21);
+      _goToHall8th();
+    } else if (name.contains("H860")) {
+      CameraPosition _currentPos =
+          CameraPosition(target: LatLng(45.49744, -73.57875), zoom: 21);
+      _goToHall8th();
+    }
+  }
+
+  Future<void> _goToHall8th() async {
+    CameraPosition _currentPos = CameraPosition(
+        bearing: 123.31752014160156,
+        target: LatLng(45.49726709926478, -73.57894677668811),
+        tilt: 0.0,
+        zoom: 19.03557586669922);
+
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(
+        CameraUpdate.newLatLng(LatLng(45.49726709926478, -73.57894677668811)));
+    controller.moveCamera(
+        CameraUpdate.newLatLng(LatLng(45.49726709926478, -73.57894677668811)));
+    controller.animateCamera(CameraUpdate.newCameraPosition(_currentPos));
+  }
+}
