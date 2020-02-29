a8ba8a8e9211f701bef5adf60882938da78a87d0
Joshua Butler <butler.joshua355@gmail.com>
2020-02-29 17:23:32 -0500

    Feature/pathfinding (#64)
    * Class models for coordinates: rooms and portals
    
    * Class model for a segment
    
    * Added function to check disability friendliness of a segment
    
    * Added function to compute length in meters of a segment
    
    * Class model for a path
    
    * Added function to check disability friendliness of a path
    
    * Added function to compute length in meters of a path
    
    * Increased sdk minimum to 2.2.0 due to use of set literals
    
    * Class model for a floor
    
    * Compute all paths from source to destination on the same floor
    
    * Find the shortest path from source to destination on a floor
    
    * Added a clear path method
    
    * Added a method to check for adjacency coordinates
    
    * Removed segment assertion, and updated conditions for disability-friendly
    
    * Added getter for path segments, and comments
    
    * Added attribute to store floor polygons, getters and setters, filter methods, and modified shortest path return type
    
    * Separation of responsibility: updated code to be more intuitive with regards to method return types and class attributes
    
    * Indoor directions with disability-friendly option completed
    
    * Combined coordinate, portal coordinate, and room coordinate into one coordinate file, and renamed files to follow effective dart styling guidelines
    
    * Polygons are lists not sets
    
    * Added missing building attribute with getter
    
    * Fixed pathfinding to account for the case that there may be no intermediary portal coordinates
    
    * Changed Coordinate class from abstract to 'public'
    
    * Fixed constructor to respect the if I am your adj you must be my adj when passing adjCoordinates
    
    * Added tests for coordinate
    
    * Added tests for segment
    
    * Refactored getCoordinatesInOrder to coordinatesInOrder, and changed return type to list. Added tests for path
    
    * Updated toString to a required unique attribute composition
    
    * Added tests for floor
    
    * Fixed set equality
    
    * Updated tests, and added tests for building
    
    * TravisCI detected a difference between actual and expected. Added room for possibly rounding or truncation error.

---
+++ lib/models/building.dart
@@ -0,0 +1,69 @@
+import 'dart:collection';
+
+import 'coordinate.dart';
+import 'floor.dart';
+import 'path.dart';
+
+class Building {
+  final String _building;
+  List<Coordinate> _polygon; //A polygon includes a duplicated point for google maps
+  Map<String, Floor> _floors = HashMap<String, Floor>();
+
+  Building(this._building, {polygon}){
+    _polygon = polygon;
+  }
+
+  String get building => _building;
+  List<Coordinate> get polygon => _polygon;
+  Map<String,Floor> get floors => _floors;
+
+  set floors(Map<String, Floor> floors) => _floors = floors;
+
+  void addFloor(Floor floor) {
+    _floors[floor.floor] = floor;
+  }
+
+  //Would template method apply here to improve extensibility?
+  //May set disability to true using optional named parameter
+  //spans at most 2 floors per building
+  Map<String, Path> shortestPath(Coordinate s, Coordinate d, {bool isDisabilityFriendly = false}) {
+    assert (s != null && d != null);
+    var indoorNavigationMap = LinkedHashMap<String, Path>();
+    var sFloor = _floors[s.floor];
+    var dFloor = _floors[d.floor];
+    if(sFloor.floor == dFloor.floor) {
+      indoorNavigationMap[s.floor] = sFloor.shortestPath(s, d);
+      return indoorNavigationMap;
+    }
+    //How should I get the closest exit/entry coordinates in a
+    // more efficient method than below? KTree/Graph method maybe?
+    //Get all valid exits
+    var exits = sFloor.validExitCoordinates(d.floor, isDisabilityFriendly: isDisabilityFriendly);
+    //Get all paths to the exits found
+    var exitPaths = <Path>[];
+    for (var exit in exits) {
+      if (s == exit) {
+        exitPaths.add(Path(<Coordinate>[s, exit]));
+      }
+      exitPaths.add(sFloor.shortestPath(s, exit));
+    }
+    //Get the shortest of all the paths found
+    var shortestExitPath = exitPaths[0];
+    for (var exitPath in exitPaths) {
+      if (exitPath.length() < shortestExitPath.length()) {
+        shortestExitPath = exitPath;
+      }
+    }
+    indoorNavigationMap[s.floor] = shortestExitPath;
+    //Find the coordinate of the destination floor from the exit found
+    var sExitAdjacentExitCoordinates = shortestExitPath.segments.last.destination.adjCoordinates;
+    var dEntry;
+    for (var sExitAdjacentCoordinate in sExitAdjacentExitCoordinates) {
+      if (sExitAdjacentCoordinate.floor == dFloor.floor) {
+        dEntry = sExitAdjacentCoordinate;
+      }
+    }
+    indoorNavigationMap[d.floor] = dFloor.shortestPath(dEntry, d);
+    return indoorNavigationMap;
+  }
+}
