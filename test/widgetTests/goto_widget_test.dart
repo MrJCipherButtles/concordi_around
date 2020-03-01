dd554d79e99fb78e68843b88d9722bf735c2a1ed
Muhammad Saad Mujtaba <muhanator@hotmail.com>
2020-03-01 18:02:46 -0500

    Revert "Added one line of test for search"
    This reverts commit 370ff222f7f9c3acfc6962a02bdc4e8af3b5e886.

--- test/widgetTests/goto_widget_test.dart
+++ test/widgetTests/goto_widget_test.dart
@@ -16,7 +16,6 @@ void main() {
     await tester.pump();
 
     //verifying all correct widgets are present
-    expect(find.text("origin..."), findsOneWidget);
     expect(find.text("destination..."), findsOneWidget);
     expect(find.byIcon(Icons.arrow_back), findsOneWidget);
 
