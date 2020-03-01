7fe77753d1626626fd04987af7b7cf35fd4d04a6
ChesterLYU <y-chester@hotmail.com>
2020-03-01 17:04:12 -0500

    - Added disability button - Added the snackbar for notification

---
+++ lib/global.dart
@@ -0,0 +1,7 @@
+library concordi_around.globals;
+
+/* 
+disabilityMode is to save the state of the toggle from the app drawer.
+it should be removed once a config file is used to save the user preferences
+*/
+bool disabilityMode = false;
