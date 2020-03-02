6bdb24544e2cd03d843df627491a9e3bd3abae51
EspressoCode <hn.84@yahoo.com>
2020-03-01 23:06:48 -0500

    modified tests to add coverage

--- test/widgetTests/goto_widget_test.dart
+++ test/widgetTests/goto_widget_test.dart
@@ -18,20 +18,8 @@ void main() {
     //verifying all correct widgets are present
     expect(find.text("Choose starting location"), findsWidgets);
     expect(find.text("Choose destination"), findsWidgets);
-    expect(find.byIcon(Icons.arrow_back), findsWidgets);
-    expect(find.byType(TextField), findsWidgets);
-    expect(find.byType(Container), findsWidgets);
-    expect(find.byType(Material), findsWidgets);
-    expect(find.byType(Column), findsWidgets);
-    expect(find.byType(Row), findsWidgets);
-    expect(find.byType(GestureDetector), findsWidgets);
-    expect(find.byType(Navigator), findsWidgets);
-    expect(find.byType(SizedBox), findsWidgets);
-    expect(find.byType(Expanded), findsWidgets);
-    expect(find.byType(Card), findsWidgets);
-    expect(find.byType(Center), findsWidgets);
-    expect(find.byType(FloatingActionButton), findsWidgets);
-    expect(find.byType(Align), findsWidgets);
+    expect(find.byType(FloatingActionButton), findsOneWidget);
+    expect(find.text('ETA (min) + Distance (m/km)'), findsOneWidget);
 
     //TODO: Find the existence of the two origin and destination search boxes
   });
