9cbc6b7bd82e057e0ca1cc009ff396687d59abac
Divesh Patel <divesh.pat3l@gmail.com>
2020-03-01 19:48:31 -0500

    Added SVG floor plan support

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
+import 'package:concordi_around/provider/map_notifier.dart';
 import 'package:concordi_around/views/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'ConcordiAround', home: HomePage());
+    return MaterialApp(
+        title: 'ConcordiAround',
+        home: ChangeNotifierProvider(
+            child: HomePage(),
+            create: (context) => MapNotifier())
+    );
   }
 }
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,12 +1,17 @@
 import 'dart:async';
 
+import 'package:concordi_around/provider/map_notifier.dart';
+import 'package:concordi_around/services/map_helper.dart';
 import 'package:concordi_around/views/go_to_page.dart';
 import 'package:concordi_around/widgets/drawer.dart';
+import 'package:concordi_around/widgets/svg_floor_plan/floor_selector_enter_building_column.dart';
 import 'package:concordi_around/widgets/search/main_search_bar.dart';
+import 'package:concordi_around/widgets/svg_floor_plan/svg_floor_plans.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:provider/provider.dart';
 
 import '../models/coordinate.dart';
 
@@ -26,6 +31,8 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    MapNotifier mapNotifier = Provider.of<MapNotifier>(context);
+
     if (_cameraPosition == null) {
       _cameraPosition = CameraPosition(target: LatLng(0, 0));
     }
@@ -48,9 +55,33 @@ class _HomePageState extends State<HomePage> {
               onMapCreated: (GoogleMapController controller) {
                 _controller.complete(controller);
               },
-              onCameraMove: (CameraPosition cameraPosition) async {},
+              onCameraMove: (CameraPosition cameraPosition) async {
+                GoogleMapController _mapController = await _controller.future;
+                if (MapHelper.isWithinHallStrictBound(cameraPosition.target) &&
+                    cameraPosition.zoom > 18) {
+                  mapNotifier.setFloorPlanVisibility(true);
+                  _setStyle(_mapController);
+                } else {
+                  mapNotifier.setFloorPlanVisibility(false);
+                  _resetStyle(_mapController);
+                }
+                if (MapHelper.isWithinHall(cameraPosition.target) &&
+                    mapNotifier.showFloorPlan == false) {
+                  mapNotifier.setEnterBuildingVisibility(true);
+                } else {
+                  mapNotifier.setEnterBuildingVisibility(false);
+                }
+              },
             )),
-            SearchBar(coordinate: (Coordinate coordinate) => {goToSpecifiedLatLng(coordinate)}),
+            SearchBar(
+                coordinate: (Coordinate coordinate) =>
+                    {goToSpecifiedLatLng(coordinate)}),
+            SVGFloorPlans(),
+            FloorSelectorEnterBuilding(
+              selectedFloor: (int floor) =>
+                  {mapNotifier.setSelectedFloor(floor)},
+              enterBuildingPressed: () => goToHallSVG(),
+            ),
           ],
         ),
         drawer: SidebarDrawer(),
@@ -148,5 +179,30 @@ class _HomePageState extends State<HomePage> {
       CameraPosition _newPosition = CameraPosition(target: coordinate.toLatLng(), zoom: 18.5);
       controller.animateCamera(CameraUpdate.newCameraPosition(_newPosition));
     }
+  }
+
+  void goToHallSVG() async { // May need return type of Future<void>, not sure yet
+    CameraPosition _currentPos = CameraPosition(
+        bearing: 123.31752014160156,
+        target: LatLng(45.49726709926478, -73.57894677668811),
+        tilt: 0.0,
+        zoom: 19.03557586669922);
+
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_currentPos));
+  }
+
+  void _setStyle(GoogleMapController controller) async {
+    print("Setting map style");
+    String value = await DefaultAssetBundle.of(context)
+        .loadString('assets/map_style.json');
+    controller.setMapStyle(value);
+  }
+
+  void _resetStyle(GoogleMapController controller) async {
+    print("Reseting");
+    String value = await DefaultAssetBundle.of(context)
+        .loadString('assets/map_style_reset.json');
+    controller.setMapStyle(value);
   }
 }
