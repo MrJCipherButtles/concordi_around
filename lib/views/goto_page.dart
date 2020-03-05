1e710cafa599428a50333510477ba03769981dfa
ChesterLYU <y-chester@hotmail.com>
2020-03-05 17:34:00 -0500

    - Added callback functions to the goto_page.dart - Fully integrated with the map.dart page - Coordinate.dart changed the toString to display the name instead of LatLng

--- lib/views/goto_page.dart
+++ lib/views/goto_page.dart
@@ -1,76 +1,188 @@
 import 'package:flutter/material.dart';
+import 'package:concordi_around/models/coordinate.dart';
+import 'package:concordi_around/widgets/search/main_search_bar.dart';
+import 'package:concordi_around/services/constants.dart' as constant;
+import 'package:geolocator/geolocator.dart';
 
 class GotoPage extends StatefulWidget {
+  final Position _current;
+  final Function(List<Coordinate>) confirmDirection;
+
+  const GotoPage(this._current, {this.confirmDirection});
+
   @override
   _GotoPageState createState() => _GotoPageState();
 }
 
 class _GotoPageState extends State<GotoPage> {
+  Coordinate _searchedStart = null;
+  Coordinate _searchedDestination = null;
+
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          body: Column(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.only(bottom: 10),
-            color: Colors.grey,
-            child: Column(
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Expanded(
-                      flex: 1,
-                      child: IconButton(
-                        onPressed: () => Navigator.pop(context),
-                        icon: Icon(Icons.arrow_back),
+        body: Column(
+          children: <Widget>[
+            Container(
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        flex: 1,
+                        child: IconButton(
+                          onPressed: () => Navigator.pop(context),
+                          icon: Icon(Icons.arrow_back),
+                        ),
                       ),
-                    ),
-                    Expanded(
-                      flex: 8,
-                      child: Column(
-                        children: <Widget>[
-                          TextField(
-                            decoration: InputDecoration(
-                              hintText: "Current Location",
-                              icon: Icon(Icons.my_location),
+                      Expanded(
+                        flex: 8,
+                        child: Column(
+                          children: <Widget>[
+                            TextField(
+                              decoration: InputDecoration(
+                                hintText: (_searchedStart == null)
+                                    ? "Current Location"
+                                    : _searchedStart.toString(),
+                                icon: Icon(Icons.my_location),
+                              ),
+                              readOnly: true,
+                              onTap: () {
+                                showSearch(
+                                  context: context,
+                                  delegate: PositionedFloatingSearchBar(
+                                      coordinate: (Coordinate val) {
+                                    setState(() {
+                                      _searchedStart = val;
+                                    });
+                                  }),
+                                );
+                              },
                             ),
-                            enabled: false,
-                          ),
-                          TextField(
-                            decoration: InputDecoration(
-                              hintText: "Enter Destination",
-                              icon: Icon(Icons.location_on),
+                            TextField(
+                              decoration: InputDecoration(
+                                hintText: (_searchedDestination == null)
+                                    ? "Enter Destination"
+                                    : _searchedDestination.toString(),
+                                icon: Icon(Icons.location_on),
+                              ),
+                              readOnly: true,
+                              onTap: () {
+                                showSearch(
+                                  context: context,
+                                  delegate: PositionedFloatingSearchBar(
+                                      coordinate: (Coordinate val) {
+                                    setState(() {
+                                      _searchedDestination = val;
+                                    });
+                                  }),
+                                );
+                              },
                             ),
-                            enabled: false,
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: IconButton(
-                        onPressed: null,
-                        icon: Icon(Icons.swap_vert),
+                      Expanded(
+                        flex: 1,
+                        child: IconButton(
+                          onPressed: null,
+                          icon: Icon(Icons.swap_vert),
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    IconButton(
-                        icon: Icon(Icons.directions_car),
-                        color: Colors.grey,
-                        onPressed: () {
-                          Navigator.pop(context);
-                        })
-                  ],
-                )
-              ],
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Expanded(flex: 1, child: Container()),
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          margin:
+                              EdgeInsets.only(top: 5, left: 2.5, right: 2.5),
+                          decoration: BoxDecoration(
+                              color: constant.COLOR_CONCORDIA,
+                              borderRadius: BorderRadius.circular(
+                                  constant.BORDER_RADIUS)),
+                          child: IconButton(
+                              icon: Icon(Icons.directions_car),
+                              color: Colors.white,
+                              onPressed: () {
+                                Navigator.pop(context);
+                              }),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: IconButton(
+                            icon: Icon(Icons.directions_transit),
+                            color: Colors.black,
+                            onPressed: () {
+                              Navigator.pop(context);
+                            }),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: IconButton(
+                            icon: Icon(Icons.directions_bike),
+                            color: Colors.black,
+                            onPressed: () {
+                              Navigator.pop(context);
+                            }),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: IconButton(
+                            icon: Icon(Icons.directions_walk),
+                            color: Colors.black,
+                            onPressed: () {
+                              Navigator.pop(context);
+                            }),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Container(),
+                      ),
+                    ],
+                  ),
+                  Divider(),
+                ],
+              ),
             ),
-          )
-        ],
-      )),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+            backgroundColor: constant.COLOR_CONCORDIA,
+            foregroundColor: Colors.white,
+            child: Text("GO"),
+            onPressed: () {
+              _searchedDestination == null
+                  ? _showToast(context)
+                  : {
+                      _searchedStart == null
+                          ? widget.confirmDirection(new List<Coordinate>.from([
+                              widget._current,
+                              _searchedDestination
+                            ])) //this doesn't work because of outside isnt implemented yet
+                          : widget.confirmDirection(new List<Coordinate>.from(
+                              [_searchedStart, _searchedDestination])),
+                      Navigator.pop(context)
+                    };
+            }),
+      ),
     );
+  }
+
+
+  //The snackbar is current not working but is a QoL implementation, will be added as a bug
+  void _showToast(BuildContext context) {
+    final scaffold = Scaffold.of(context);
+    scaffold.removeCurrentSnackBar();
+    scaffold.showSnackBar(SnackBar(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(15), topRight: Radius.circular(15))),
+      content: Text("Destination is not set"),
+    ));
   }
 }
