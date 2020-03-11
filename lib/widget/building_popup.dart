148c4072312754ab25a90f7d9425fd1d0d1335d5
Raffi Jansezian <rjansezian@gmail.com>
2020-03-11 17:09:52 -0400

    Added slide up panel widget

---
+++ lib/widget/building_popup.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+
+class BuildingPopup extends StatefulWidget {
+  State<StatefulWidget> createState() {
+    return _BuildingPopupState();
+  }
+}
+
+class _BuildingPopupState extends State<BuildingPopup> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Visibility(
+      maintainSize: true,
+      maintainAnimation: true,
+      maintainState: true,
+      child:Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SlidingUpPanel(
+              backdropEnabled: true,
+              panel: Center(
+                child: Text(
+                  "Building Info"
+                ),
+              ),
+              collapsed: Container(
+                padding: EdgeInsets.all(20),
+                child: Row(
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Container(
+                            child: Text(
+                              "Pavillion Henry F. Hall Building",
+                              style: TextStyle(fontSize: 22),
+                            ),
+                            ),
+                          ),
+                        Text(
+                          "Boulevard de Maisonneuve O, Montreal, QC H3G 1M8",
+                          style: TextStyle(fontSize: 12.5, color: Colors.grey),
+                        ) 
+                      ],
+                    ),
+                  ],
+                ) 
+                ),
+          )
+          ],
+        )
+      ));
+  }
+}
