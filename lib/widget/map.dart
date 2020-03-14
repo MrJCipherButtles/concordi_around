a2a6387ca99188e2584fe577c0f930072dd6a697
Umer Anwar <anwarumer97@gmail.com>
2020-03-14 16:18:13 -0400

    Feature/floor polygon (#86)
    * added helper function in building singleton and 9th floor polygon
    
    * fixed null check
    
    * added 9th floor polygon
    
    * added floor plan polygon change on maps widget
    
    * change path line when changing floors
    
    * refactored code
    
    * changed hall to H
    
    * Added markers for each classroom
    
    * refactored building_singleton to move data points to data_points.dart
    
    * further refactored building_singleton
    
    * refactored building list to map
    
    * added marker
    
    * remove markers when building not in focus
    
    * Markers are no longer visible when zooming out of the map
    
    * Added spacing under floor selector
    
    * fixed floor polygons
    
    * added ninth floor markers
    
    * added different icons
    
    * Added marker helper file
    
    * refactored code to marker helper class
    
    * fixed failing test and refactored code
    
    Co-authored-by: Divesh Patel <divesh.pat3l@gmail.com>

--- lib/widget/map.dart
+++ lib/widget/map.dart
@@ -3,15 +3,17 @@ import 'dart:async';
 import 'package:concordi_around/data/building_singleton.dart';
 import 'package:concordi_around/model/building.dart';
 import 'package:concordi_around/model/coordinate.dart';
+import 'package:concordi_around/model/path.dart';
 import 'package:concordi_around/provider/direction_notifier.dart';
 import 'package:concordi_around/provider/map_notifier.dart';
 import 'package:concordi_around/service/map_constant.dart' as constant;
 import 'package:concordi_around/service/map_helper.dart';
+import 'package:concordi_around/service/marker_helper.dart';
+import 'package:concordi_around/service/polygon_helper.dart';
 import 'package:concordi_around/view/goto_page_new.dart';
 import 'package:concordi_around/widget/direction_panel.dart';
 import 'package:concordi_around/widget/search/main_search_bar.dart';
 import 'package:concordi_around/widget/svg_floor_plan/floor_selector_enter_building_column.dart';
-import 'package:concordi_around/widget/svg_floor_plan/svg_floor_plans.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -32,14 +34,22 @@ class _MapState extends State<Map> {
   Position _position;
   CameraPosition _cameraPosition;
   StreamSubscription _positionStream;
-
+  MarkerHelper markerHelper;
+  PolygonHelper polygonHelper;
   Set<Polyline> direction;
   Set<Polygon> buildingHighlights;
+  Set<Marker> mapMarkers = {};
+
+
+  var shortestPath;
 
   @override
   void initState() {
     super.initState();
     buildingHighlights = BuildingSingleton().getOutdoorBuildingHighlights();
+    polygonHelper = PolygonHelper();
+    markerHelper = MarkerHelper();
+    buildingHighlights.addAll(polygonHelper.getFloorPolygon(9));
     _geolocator = Geolocator()..forceAndroidLocationManager;
     LocationOptions locationOptions = LocationOptions(
         accuracy: LocationAccuracy.bestForNavigation, distanceFilter: 1);
@@ -88,25 +98,22 @@ class _MapState extends State<Map> {
           zoomGesturesEnabled: true,
           polygons: buildingHighlights,
           polylines: direction,
+          markers: mapMarkers,
           initialCameraPosition: _cameraPosition,
           onMapCreated: (GoogleMapController controller) {
             _completer.complete(controller);
           },
           onCameraMove: (CameraPosition cameraPosition) async {
             GoogleMapController _mapController = await _completer.future;
-            if (MapHelper.isWithinHallStrictBound(cameraPosition.target) &&
-                cameraPosition.zoom > constant.CAMERA_DEFAULT_ZOOM) {
+            if (MapHelper.isWithinHall(cameraPosition.target) &&
+                cameraPosition.zoom >= 18.5) {
               mapNotifier.setFloorPlanVisibility(true);
               _setStyle(_mapController);
+              mapMarkers.addAll(
+                  markerHelper.getFloorMarkers(mapNotifier.selectedFloorPlan));
             } else {
               mapNotifier.setFloorPlanVisibility(false);
               _resetStyle(_mapController);
-            }
-            if (MapHelper.isWithinHall(cameraPosition.target) &&
-                mapNotifier.showFloorPlan == false) {
-              mapNotifier.setEnterBuildingVisibility(true);
-            } else {
-              mapNotifier.setEnterBuildingVisibility(false);
             }
             mapNotifier.setCampusLatLng(cameraPosition.target);
           },
@@ -172,9 +179,9 @@ class _MapState extends State<Map> {
                   Provider.of<MapNotifier>(context, listen: false)
                       .goToSpecifiedLatLng(futureCoordinate: coordinate)
                 }),
-        SVGFloorPlans(),
         FloorSelectorEnterBuilding(
-          selectedFloor: (int floor) => {mapNotifier.setSelectedFloor(floor)},
+          selectedFloor: (int floor) =>
+              {updateFloor(floor), mapNotifier.setSelectedFloor(floor)},
           enterBuildingPressed: () => mapNotifier.goToHallSVG(),
         ),
         DirectionPanel(),
@@ -182,18 +189,40 @@ class _MapState extends State<Map> {
     );
   }
 
+  void updateFloor(int floor) {
+    setState(() {
+      if (shortestPath != null) {
+        Path path = shortestPath['$floor'];
+        if (path != null) {
+          direction = {path.toPolyline()};
+        } else {
+          direction = {};
+        }
+      }
+      if (floor == 9) {
+        buildingHighlights.removeAll(polygonHelper.getFloorPolygon(8));
+        mapMarkers.removeAll(markerHelper.getFloorMarkers(8));
+      } else if (floor == 8) {
+        buildingHighlights.removeAll(polygonHelper.getFloorPolygon(9));
+        mapMarkers.removeAll(markerHelper.getFloorMarkers(9));
+      }
+      mapMarkers.addAll(markerHelper.getFloorMarkers(floor));
+      buildingHighlights.addAll(polygonHelper.getFloorPolygon(floor));
+    });
+  }
+
   void _setStyle(GoogleMapController controller) async {
-    print("Setting map style");
     String value = await DefaultAssetBundle.of(context)
         .loadString('assets/map_style.json');
     controller.setMapStyle(value);
   }
 
   void _resetStyle(GoogleMapController controller) async {
-    print("Reseting");
     String value = await DefaultAssetBundle.of(context)
         .loadString('assets/map_style_reset.json');
     controller.setMapStyle(value);
+    mapMarkers.removeAll(markerHelper.getFloorMarkers(8));
+    mapMarkers.removeAll(markerHelper.getFloorMarkers(9));
   }
 
   void goToCurrent() async {
@@ -207,13 +236,9 @@ class _MapState extends State<Map> {
   void drawShortestPath(
       Coordinate start, Coordinate end, bool isDisabilityEnabled) {
     BuildingSingleton buildingSingleton = new BuildingSingleton();
-    Building hall;
-    for (var building in buildingSingleton.buildings) {
-      if (building.building.contains("Hall")) {
-        hall = building;
-      }
-    }
-    var shortestPath = hall.shortestPath(start, end,
+    Building hall = buildingSingleton.buildings['H'];
+
+    shortestPath = hall.shortestPath(start, end,
         isDisabilityFriendly: isDisabilityEnabled);
     // TODO: setState of direction should be set by listening to selectedFloor MapNotifier instead of hardcoded '9'
     setState(() {
