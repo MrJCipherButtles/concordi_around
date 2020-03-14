2f742bf9e830b246a0856ccf12652ae01a73e1f7
Joshua Butler <butler.joshua355@gmail.com>
2020-03-14 16:28:52 -0400

    removed new keywords and formatted API directions request as per Google's developer guide

--- lib/service/map_direction.dart
+++ lib/service/map_direction.dart
@@ -1,29 +1,22 @@
 import 'dart:convert';
 
 import 'package:concordi_around/model/direction.dart';
-import 'package:concordi_around/model/list_item.dart';
 import 'package:dio/dio.dart';
-import 'package:uuid/uuid.dart';
 
 import '../credential.dart';
 
 class MapDirection {
-
-  var uuid = Uuid();
-  String _sessionToken;
-
-    // Google Directions API requests
-  Future<List<ListItem>> _getDirection(String origin, String destination) async {
+  // Google Directions API requests
+  Future<Direction> _getDirection(String origin, String destination,
+      [String mode = 'driving']) async {
     if (origin.isEmpty || destination.isEmpty) {
-      throw("Origin or destination is empty");
+      throw ("Origin or destination is empty");
     }
-    if (_sessionToken == null) {
-      _sessionToken = uuid.v4();
-    }
-    
+
     String baseURL = 'https://maps.googleapis.com/maps/api/directions/json';
 
-    String request = '$baseURL?origin=$origin&destination=$destination&key=$PLACES_API_KEY&sessiontoken=$_sessionToken';
+    String request =
+        '$baseURL?origin=$origin&destination=$destination&mode=$mode&key=$DIRECTIONS_API_KEY';
     Response response = await Dio().get(request);
 
     print(response.data);
@@ -31,7 +24,6 @@ class MapDirection {
     final jsonResponse = json.decode(response.data);
     Direction direction = new Direction.fromJson(jsonResponse);
 
-    print(direction);
-
+    return direction;
   }
 }
