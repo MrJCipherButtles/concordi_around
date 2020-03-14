9dc29eaf8a8f004c1d4fb55421659a15ea88007a
Mehrdad Ahmadi <mehrdad2maskull@gmail.com>
2020-03-14 17:20:31 -0400

    WIP: Adding route modes

--- lib/service/map_direction.dart
+++ lib/service/map_direction.dart
@@ -6,21 +6,22 @@ import 'package:dio/dio.dart';
 import '../credential.dart';
 
 class MapDirection {
-  // Google Directions API requests
-  Future<Direction> _getDirection(String origin, String destination,
-      [String mode = 'driving']) async {
+
+    // Google Directions API requests
+  Direction getDirection(String origin, String destination, String mode) {
     if (origin.isEmpty || destination.isEmpty) {
       throw ("Origin or destination is empty");
     }
 
     String baseURL = 'https://maps.googleapis.com/maps/api/directions/json';
 
-    String request =
-        '$baseURL?origin=$origin&destination=$destination&mode=$mode&key=$DIRECTIONS_API_KEY';
+    String request = '$baseURL?origin=$origin&destination=$destination&mode=$mode&key=$DIRECTIONS_API_KEY';
     Response response = await Dio().get(request);
 
     final jsonResponse = json.decode(response.data);
     Direction direction = Direction.fromJson(jsonResponse);
+
+    print(direction);
 
     return direction;
   }
