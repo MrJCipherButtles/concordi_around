3a661469cb8e857213b5ee6ea4c0ea0acac6a1e2
Divesh Patel <divesh.pat3l@gmail.com>
2020-03-15 00:20:25 -0400

    Added back credential file

---
+++ lib/credential.dart
@@ -0,0 +1,10 @@
+// Disclaimer: You should never version control your API keys
+// Such an oversight can potentially be costly $$$
+// Once this file is updated with your API keys on your local repo,
+// run the following cmd in the project directory:
+// git update-index --assume-unchanged lib/credential.dart
+
+// TODO: Enter your own API keys
+const PLACES_API_KEY = 'AIzaSyCCBLkq2uPHmJ-sXYVrXGo_nMntjf2s_qY';
+const DIRECTIONS_API_KEY = 'AIzaSyCCBLkq2uPHmJ-sXYVrXGo_nMntjf2s_qY';
+const CALENDAR_API_KEY = 'AIzaSyCCBLkq2uPHmJ-sXYVrXGo_nMntjf2s_qY';
