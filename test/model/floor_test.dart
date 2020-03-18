52ecaba07fcda34ac698b6ad2d2bcc6195d027d2
Umer Anwar <anwarumer97@gmail.com>
2020-03-18 18:16:51 -0400

    Feature/data points (#112)
    * added all 8th floor points
    
    * Feature/direction (#105)
    
    * Direction Feature
    
    Added direction data model and an API call to use Google Places
    
    * removed new keywords and formatted API directions request as per Google's developer guide
    
    * Added callback
    
    * Added plugin
    
    * Fixed syntax error
    
    * Added directions line on map
    
    * Added directions lines
    
    * Added direction support for all modes
    
    * Ran flutter format
    
    * Removed duplicate imports
    
    * Fix tests to reflect the importance of dynamic type in the shortest path algorithm
    
    * Optimized shortest path algorithm to not traverse any RoomCoordinate. This reduces performance from the order of seconds to milliseconds
    
    * connected floors
    
    * fixed adjacency
    
    * fixed 9th f
    
    * ran flutter format
    
    * remove commented rooms
    
    Co-authored-by: Divesh Patel <46823998+DiveshPatel@users.noreply.github.com>
    Co-authored-by: Joshua Butler <butler.joshua355@gmail.com>

--- test/model/floor_test.dart
+++ test/model/floor_test.dart
@@ -5,16 +5,16 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('shortest path', () {
-    var a = Coordinate(45.49719, -73.57933, '1', 'Hall', 'SGW');
-    var b = Coordinate(45.49735, -73.57918, '1', 'Hall', 'SGW');
-    var c = Coordinate(45.49755, -73.57899, '1', 'Hall', 'SGW');
-    var d = Coordinate(45.49734, -73.57855, '1', 'Hall', 'SGW');
-    var e = Coordinate(45.49714, -73.57875, '1', 'Hall', 'SGW');
-    var f = Coordinate(45.49698, -73.57888, '1', 'Hall', 'SGW');
-    var x = Coordinate(45.49720, -73.57887, '1', 'Hall', 'SGW');
-    var y = Coordinate(45.49741, -73.57868, '1', 'Hall', 'SGW');
-    var z = Coordinate(45.49748, -73.57885, '1', 'Hall', 'SGW');
-    var zz = Coordinate(45.49753, -73.57893, '1', 'Hall', 'SGW');
+    var a = PortalCoordinate(45.49719, -73.57933, '1', 'Hall', 'SGW');
+    var b = PortalCoordinate(45.49735, -73.57918, '1', 'Hall', 'SGW');
+    var c = PortalCoordinate(45.49755, -73.57899, '1', 'Hall', 'SGW');
+    var d = PortalCoordinate(45.49734, -73.57855, '1', 'Hall', 'SGW');
+    var e = RoomCoordinate(45.49714, -73.57875, '1', 'Hall', 'SGW');
+    var f = RoomCoordinate(45.49698, -73.57888, '1', 'Hall', 'SGW');
+    var x = PortalCoordinate(45.49720, -73.57887, '1', 'Hall', 'SGW');
+    var y = PortalCoordinate(45.49741, -73.57868, '1', 'Hall', 'SGW');
+    var z = PortalCoordinate(45.49748, -73.57885, '1', 'Hall', 'SGW');
+    var zz = PortalCoordinate(45.49753, -73.57893, '1', 'Hall', 'SGW');
 
     a.adjCoordinates = {f, b};
     b.adjCoordinates = {a, x, c};
@@ -27,12 +27,12 @@ void main() {
     z.adjCoordinates = {c, y};
     zz.adjCoordinates = {c, y};
 
-
     var floor = Floor('1');
     floor.coordinates = {a, b, c, d, e, f, x, y, z, zz};
-    
+
     test('shortest path from f to e', () {
-      expect(floor.shortestPath(f, e).toString(), Path([f, a, b, c, y, d, e]).toString());
+      expect(floor.shortestPath(f, e).toString(),
+          Path([f, a, b, c, y, d, e]).toString());
     });
     test('shortest path from a to a', () {
       expect(floor.shortestPath(a, a).length(), closeTo(0, 0.1));
@@ -50,30 +50,29 @@ void main() {
     var f = Coordinate(45.49698, -73.57888, '1', 'Hall', 'SGW', type: 'C');
     var x = Coordinate(45.49720, -73.57887, '1', 'Hall', 'SGW', type: 'A');
     var y = Coordinate(45.49741, -73.57868, '1', 'Hall', 'SGW', type: 'B');
-    
 
-    a.adjCoordinates = {f,b};
-    b.adjCoordinates = {a,x,c};
-    c.adjCoordinates = {b,y};
-    d.adjCoordinates = {y,e};
+    a.adjCoordinates = {f, b};
+    b.adjCoordinates = {a, x, c};
+    c.adjCoordinates = {b, y};
+    d.adjCoordinates = {y, e};
     e.adjCoordinates = {d};
     f.adjCoordinates = {a};
-    x.adjCoordinates = {b,y};
-    y.adjCoordinates = {c,x,d};
+    x.adjCoordinates = {b, y};
+    y.adjCoordinates = {c, x, d};
 
     var floor = Floor('1');
-    floor.coordinates = {a,b,c,d,e,f,x,y};
+    floor.coordinates = {a, b, c, d, e, f, x, y};
 
     test('should get all A type coordinates', () {
-      expect(floor.coordinatesByGivenTypes(['A']), [a,d,x]);
+      expect(floor.coordinatesByGivenTypes(['A']), [a, d, x]);
     });
     test('should get all A and C type coordinates', () {
-      expect(floor.coordinatesByGivenTypes(['A','C']), [a,c,d,f,x]);
+      expect(floor.coordinatesByGivenTypes(['A', 'C']), [a, c, d, f, x]);
     });
   });
-  group('valid exit coordinates', ()
-  {
-    var a = PortalCoordinate(45.49719, -73.57933, '1', 'Hall', 'SGW', isDisabilityFriendly: true);
+  group('valid exit coordinates', () {
+    var a = PortalCoordinate(45.49719, -73.57933, '1', 'Hall', 'SGW',
+        isDisabilityFriendly: true);
     var b = PortalCoordinate(45.49735, -73.57918, '1', 'Hall', 'SGW');
     var c = PortalCoordinate(45.49755, -73.57899, '1', 'Hall', 'SGW');
     var d = Coordinate(45.49734, -73.57855, '1', 'Hall', 'SGW');
@@ -82,7 +81,8 @@ void main() {
     var x = Coordinate(45.49720, -73.57887, '1', 'Hall', 'SGW');
     var y = Coordinate(45.49741, -73.57868, '1', 'Hall', 'SGW');
 
-    var a2 = PortalCoordinate(45.49719, -73.57933, '2', 'Hall', 'SGW', isDisabilityFriendly: true);
+    var a2 = PortalCoordinate(45.49719, -73.57933, '2', 'Hall', 'SGW',
+        isDisabilityFriendly: true);
     var b2 = PortalCoordinate(45.49735, -73.57918, '2', 'Hall', 'SGW');
     var c2 = PortalCoordinate(45.49755, -73.57899, '2', 'Hall', 'SGW');
 
@@ -101,7 +101,7 @@ void main() {
     floor1.coordinates = {a, b, c, d, e, f, x, y};
     floor2.coordinates = {a2, b2, c2};
     test('should get all valid exit coordinates, disability = false', () {
-      expect(floor1.validExitCoordinates('2'), [a,b,c]);
+      expect(floor1.validExitCoordinates('2'), [a, b, c]);
     });
     test('should get all valid exit coordinates, disability = true', () {
       expect(floor1.validExitCoordinates('2', isDisabilityFriendly: true), [a]);
