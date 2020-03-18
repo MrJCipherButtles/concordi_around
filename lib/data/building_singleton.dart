59480649753271c0a329149c517dcbc5c9e4ff65
Umer <anwarumer97@gmail.com>
2020-03-17 21:38:38 -0400

    fixed 9th f

--- lib/data/building_singleton.dart
+++ lib/data/building_singleton.dart
@@ -79,7 +79,7 @@ class BuildingSingleton {
               Random rnd = Random();
               result.add(Polygon(
                   polygonId:
-                      PolygonId('${floor.floor}-${rnd.nextInt(100000000)}'),
+                  PolygonId('${floor.floor}-${rnd.nextInt(100000000)}'),
                   points: points,
                   fillColor: COLOR_CONCORDIA.withOpacity(0.4),
                   strokeWidth: 3,
@@ -101,7 +101,7 @@ class BuildingSingleton {
         if (building.floors != null) {
           var floor = building.floors[floorName];
           List<RoomCoordinate> markers =
-              floor.coordinatesByGivenTypes({'MARKER'});
+          floor.coordinatesByGivenTypes({'MARKER'});
           markers.forEach((f) => result.add(Marker(
               markerId: MarkerId(f.roomId),
               infoWindow: InfoWindow(title: f.roomId))));
@@ -114,11 +114,11 @@ class BuildingSingleton {
   void _initHall(Building hall) {
     //8th floor portals
     PortalCoordinate j8F1 = PortalCoordinate(
-        45.4971951, -73.579341, '8', 'H', 'SGW',
+        45.4971949, -73.5793413, '8', 'H', 'SGW',
         adjCoordinates: <Coordinate>{}, type: "PORTAL");
 
     PortalCoordinate j8F2 = PortalCoordinate(
-        45.497265, -73.5792743, '8', 'H', 'SGW',
+        45.4972645, -73.5792733, '8', 'H', 'SGW',
         adjCoordinates: <Coordinate>{},
         type: "PORTAL",
         isDisabilityFriendly: false);
@@ -162,7 +162,7 @@ class BuildingSingleton {
         adjCoordinates: <Coordinate>{}, type: "PORTAL");
 
     PortalCoordinate j8F11 = PortalCoordinate(
-        45.4971366, -73.5787401, '8', 'H', 'SGW',
+        45.4971364, -73.5787408, '8', 'H', 'SGW',
         adjCoordinates: <Coordinate>{}, type: "PORTAL");
 
     PortalCoordinate j8F12 = PortalCoordinate(
@@ -195,249 +195,173 @@ class BuildingSingleton {
         45.4974342, -73.5791097, '8', 'H', 'SGW',
         adjCoordinates: <Coordinate>{}, type: "PORTAL");
 
+
     //9th floor portals
     PortalCoordinate j9F1 = PortalCoordinate(
-        45.497223, -73.579356, '9', 'H', 'SGW',
+        45.4972229, -73.5793637, '9', 'H', 'SGW',
         adjCoordinates: <Coordinate>{}, type: "PORTAL");
 
     PortalCoordinate j9F2 = PortalCoordinate(
-        45.497282, -73.579296, '9', 'H', 'SGW',
+        45.4972848, -73.5793076, '9', 'H', 'SGW',
         adjCoordinates: <Coordinate>{}, type: "PORTAL");
 
     PortalCoordinate j9F3 = PortalCoordinate(
-        45.497279, -73.579218, '9', 'H', 'SGW',
+        45.4972823, -73.5792232, '9', 'H', 'SGW',
         adjCoordinates: <Coordinate>{}, type: "PORTAL");
 
     PortalCoordinate j9F4 = PortalCoordinate(
-        45.497151, -73.579210, '9', 'H', 'SGW',
+        45.4971482, -73.5792097, '9', 'H', 'SGW',
         adjCoordinates: <Coordinate>{}, type: "PORTAL");
 
     PortalCoordinate j9F5 = PortalCoordinate(
-        45.497247, -73.579120, '9', 'H', 'SGW',
-        adjCoordinates: <Coordinate>{}, type: "PORTAL");
+        45.4972032, -73.579156, '9', 'H', 'SGW',
+        adjCoordinates: <Coordinate>{}, type: "PORTAL",
+    isDisabilityFriendly: false);
 
     PortalCoordinate j9F6 = PortalCoordinate(
-        45.497305, -73.579054, '9', 'H', 'SGW',
+        45.497236, -73.5791237, '9', 'H', 'SGW',
         adjCoordinates: <Coordinate>{}, type: "PORTAL");
 
     PortalCoordinate j9F7 = PortalCoordinate(
-        45.497353, -73.579043, '9', 'H', 'SGW',
+        45.4969877, -73.5788817, '9', 'H', 'SGW',
         adjCoordinates: <Coordinate>{}, type: "PORTAL");
 
     PortalCoordinate j9F8 = PortalCoordinate(
-        45.497390, -73.579003, '9', 'H', 'SGW',
-        adjCoordinates: <Coordinate>{}, type: "PORTAL");
+        45.4970442, -73.5788285, '9', 'H', 'SGW',
+        adjCoordinates: <Coordinate>{}, type: "PORTAL",
+        isDisabilityFriendly: false);
 
     PortalCoordinate j9F9 = PortalCoordinate(
-        45.497422, -73.579075, '9', 'H', 'SGW',
+        45.4971508, -73.5787267, '9', 'H', 'SGW',
         adjCoordinates: <Coordinate>{}, type: "PORTAL");
 
     PortalCoordinate j9F10 = PortalCoordinate(
-        45.497456, -73.579144, '9', 'H', 'SGW',
+        45.4973393, -73.5785469, '9', 'H', 'SGW',
         adjCoordinates: <Coordinate>{}, type: "PORTAL");
 
     PortalCoordinate j9F11 = PortalCoordinate(
-        45.497488, -73.579169, '9', 'H', 'SGW',
+        45.4974085, -73.5786857, '9', 'H', 'SGW',
         adjCoordinates: <Coordinate>{}, type: "PORTAL");
 
     PortalCoordinate j9F12 = PortalCoordinate(
-        45.497650, -73.579018, '9', 'H', 'SGW',
-        adjCoordinates: <Coordinate>{}, type: "PORTAL");
+        45.4973589, -73.5787337, '9', 'H', 'SGW',
+        adjCoordinates: <Coordinate>{}, type: "PORTAL",
+        isDisabilityFriendly: false);
 
     PortalCoordinate j9F13 = PortalCoordinate(
-        45.497562, -73.578834, '9', 'H', 'SGW',
-        adjCoordinates: <Coordinate>{}, type: "PORTAL");
+        45.4973184, -73.5787723, '9', 'H', 'SGW',
+        adjCoordinates: <Coordinate>{}, type: "PORTAL",
+        isDisabilityFriendly: true);
 
     PortalCoordinate j9F14 = PortalCoordinate(
-        45.497503, -73.578712, '9', 'H', 'SGW',
+        45.4972188, -73.5788677, '9', 'H', 'SGW',
         adjCoordinates: <Coordinate>{}, type: "PORTAL");
 
     PortalCoordinate j9F15 = PortalCoordinate(
-        45.497111, -73.57118, '9', 'H', 'SGW',
+        45.4972611, -73.5789558, '9', 'H', 'SGW',
         adjCoordinates: <Coordinate>{}, type: "PORTAL");
 
     PortalCoordinate j9F16 = PortalCoordinate(
-        45.497042, -73.579145, '9', 'H', 'SGW',
-        adjCoordinates: <Coordinate>{}, type: "PORTAL");
+        45.4972815, -73.5789366, '9', 'H', 'SGW',
+        adjCoordinates: <Coordinate>{}, type: "PORTAL",
+        isDisabilityFriendly: false);
 
     PortalCoordinate j9F17 = PortalCoordinate(
-        45.496957, -73.578869, '9', 'H', 'SGW',
+        45.4973084, -73.5790534, '9', 'H', 'SGW',
         adjCoordinates: <Coordinate>{}, type: "PORTAL");
 
     PortalCoordinate j9F18 = PortalCoordinate(
-        45.496972, -73.578892, '9', 'H', 'SGW',
+        45.4973231, -73.5790391, '9', 'H', 'SGW',
         adjCoordinates: <Coordinate>{}, type: "PORTAL");
 
     PortalCoordinate j9F19 = PortalCoordinate(
-        45.497154, -73.578731, '9', 'H', 'SGW',
+        45.4973374, -73.5790509, '9', 'H', 'SGW',
         adjCoordinates: <Coordinate>{}, type: "PORTAL");
 
     PortalCoordinate j9F20 = PortalCoordinate(
-        45.497352, -73.578536, '9', 'H', 'SGW',
+        45.4973767, -73.5790131, '9', 'H', 'SGW',
         adjCoordinates: <Coordinate>{}, type: "PORTAL");
 
     PortalCoordinate j9F21 = PortalCoordinate(
-        45.497334, -73.578493, '9', 'H', 'SGW',
-        adjCoordinates: <Coordinate>{}, type: "PORTAL");
+        45.4973955, -73.5790524, '9', 'H', 'SGW',
+        adjCoordinates: <Coordinate>{}, type: "PORTAL",
+        isDisabilityFriendly: true);
 
     PortalCoordinate j9F22 = PortalCoordinate(
-        45.497424, -73.578703, '9', 'H', 'SGW',
+        45.4974316, -73.5791262, '9', 'H', 'SGW',
         adjCoordinates: <Coordinate>{}, type: "PORTAL");
 
     PortalCoordinate j9F23 = PortalCoordinate(
-        45.497409, -73.578684, '9', 'H', 'SGW',
+        45.4974894, -73.5791726, '9', 'H', 'SGW',
         adjCoordinates: <Coordinate>{}, type: "PORTAL");
 
     PortalCoordinate j9F24 = PortalCoordinate(
-        45.497246, -73.578840, '9', 'H', 'SGW',
-        adjCoordinates: <Coordinate>{}, type: "PORTAL");
-
-    PortalCoordinate j9F25 = PortalCoordinate(
-        45.497218, -73.578863, '9', 'H', 'SGW',
-        adjCoordinates: <Coordinate>{}, type: "PORTAL");
-
-    PortalCoordinate j9F26 = PortalCoordinate(
-        45.497289, -73.579012, '9', 'H', 'SGW',
-        adjCoordinates: <Coordinate>{}, type: "PORTAL");
-
-    PortalCoordinate j9F27 = PortalCoordinate(
-        45.497300, -73.579003, '9', 'H', 'SGW',
-        adjCoordinates: <Coordinate>{},
-        type: "PORTAL",
-        isDisabilityFriendly: false);
-
-    PortalCoordinate j9F28 = PortalCoordinate(
-        45.497191, -73.579165, '9', 'H', 'SGW',
-        adjCoordinates: <Coordinate>{},
-        type: "PORTAL",
-        isDisabilityFriendly: false);
-
-    PortalCoordinate j9F29 = PortalCoordinate(
-        45.4970442, -73.5788285, '9', 'H', 'SGW',
-        adjCoordinates: <Coordinate>{},
-        type: "PORTAL",
-        isDisabilityFriendly: false);
-
-    PortalCoordinate j9F30 = PortalCoordinate(
-        45.497442, -73.578995, '9', 'H', 'SGW',
-        adjCoordinates: <Coordinate>{},
-        type: "PORTAL",
-        isDisabilityFriendly: false);
-
-    PortalCoordinate j9F31 = PortalCoordinate(
-        45.497408, -73.579041, '9', 'H', 'SGW',
-        adjCoordinates: <Coordinate>{},
-        type: "PORTAL",
-        isDisabilityFriendly: true);
-
-    PortalCoordinate j9F32 = PortalCoordinate(
-        45.497084, -73.579120, '9', 'H', 'SGW',
-        adjCoordinates: <Coordinate>{}, type: "PORTAL");
-
-    PortalCoordinate j9F33 = PortalCoordinate(
-        45.497077, -73.579111, '9', 'H', 'SGW',
-        adjCoordinates: <Coordinate>{}, type: "PORTAL");
-
-    PortalCoordinate j9F34 = PortalCoordinate(
-        45.497052, -73.579032, '9', 'H', 'SGW',
-        adjCoordinates: <Coordinate>{}, type: "PORTAL");
-
-    PortalCoordinate j9F35 = PortalCoordinate(
-        45.497207, -73.578683, '9', 'H', 'SGW',
-        adjCoordinates: <Coordinate>{}, type: "PORTAL");
-
-    PortalCoordinate j9F36 = PortalCoordinate(
-      45.497319,
-      -73.578772,
-      '9',
-      'H',
-      'SGW',
-      adjCoordinates: <Coordinate>{},
-      type: "PORTAL",
-      isDisabilityFriendly: true,
-    );
-
-    PortalCoordinate j9F37 = PortalCoordinate(
-        45.497364, -73.578729, '9', 'H', 'SGW',
-        type: "PORTAL",
-        adjCoordinates: <Coordinate>{},
-        isDisabilityFriendly: false);
+        45.4974402, -73.5789529, '9', 'H', 'SGW',
+        adjCoordinates: <Coordinate>{}, type: "PORTAL",
+    isDisabilityFriendly: false);
 
     j8F1.adjCoordinates = {j8F2, j8F15};
-    j8F2.adjCoordinates = {j8F1, j8F3, j9F28};
-    j8F3.adjCoordinates = {j8F2, j8F17};
-    j8F4.adjCoordinates = {j8F5, j8F17, j9F30};
+    j8F2.adjCoordinates = {j8F1, j8F3, j9F5};
+    j8F3.adjCoordinates = {j8F2, j8F13, j8F17};
+    j8F4.adjCoordinates = {j8F5, j8F17, j9F24};
     j8F5.adjCoordinates = {j8F4, j8F7};
-    j8F6.adjCoordinates = {j8F17, j9F31};
+    j8F6.adjCoordinates = {j8F17, j9F21};
     j8F7.adjCoordinates = {j8F5, j8F8, j8F9};
     j8F8.adjCoordinates = {j8F7, j8F11};
-    j8F9.adjCoordinates = {j8F7, j8F12, j9F37};
+    j8F9.adjCoordinates = {j8F7, j8F12, j9F12};
     j8F10.adjCoordinates = {j8F11, j8F12, j8F13};
     j8F11.adjCoordinates = {j8F8, j8F10, j8F14};
-    j8F12.adjCoordinates = {j8F9, j8F10, j9F36};
+    j8F12.adjCoordinates = {j8F9, j8F10, j9F13};
     j8F13.adjCoordinates = {j8F3, j8F10, j8F16};
-    j8F14.adjCoordinates = {j8F11, j8F15, j9F29};
+    j8F14.adjCoordinates = {j8F11, j8F15, j9F8};
     j8F15.adjCoordinates = {j8F14, j8F1};
-    j8F16.adjCoordinates = {j8F13, j9F27};
+    j8F16.adjCoordinates = {j8F13, j9F16};
     j8F17.adjCoordinates = {j8F3, j8F4, j8F6};
 
     j9F1.adjCoordinates = {j9F2, j9F4};
     j9F2.adjCoordinates = {j9F3, j9F1};
-    j9F3.adjCoordinates = {j9F2, j9F5};
-    j9F4.adjCoordinates = {j9F28, j9F1};
-    j9F5.adjCoordinates = {j9F6, j9F28, j9F3};
-    j9F6.adjCoordinates = {j9F7, j9F5, j9F26};
-    j9F7.adjCoordinates = {j9F8, j9F6};
-    j9F8.adjCoordinates = {j9F7, j9F31, j9F30};
-    j9F9.adjCoordinates = {j9F31, j9F10};
-    j9F10.adjCoordinates = {j9F9, j9F11};
+    j9F3.adjCoordinates = {j9F2, j9F6};
+    j9F4.adjCoordinates = {j9F1, j9F5, j9F7};
+    j9F5.adjCoordinates = {j9F4, j9F6, j8F2};
+    j9F6.adjCoordinates = {j9F3, j9F5, j9F17};
+    j9F7.adjCoordinates = {j9F4, j9F8};
+    j9F8.adjCoordinates = {j9F7, j9F9, j8F14};
+    j9F9.adjCoordinates = {j9F8, j9F14, j9F10};
+    j9F10.adjCoordinates = {j9F11, j9F9};
     j9F11.adjCoordinates = {j9F10, j9F12};
-    j9F12.adjCoordinates = {j9F11, j9F13};
-    j9F13.adjCoordinates = {j9F12, j9F14, j9F30};
-    j9F14.adjCoordinates = {j9F13};
-    j9F15.adjCoordinates = {j9F32, j9F4};
-    j9F16.adjCoordinates = {j9F33};
-    j9F17.adjCoordinates = {j9F18};
-    j9F18.adjCoordinates = {j9F29, j9F34, j9F17};
-    j9F19.adjCoordinates = {j9F35, j9F25, j9F29};
-    j9F20.adjCoordinates = {j9F21, j9F23, j9F35};
-    j9F21.adjCoordinates = {j9F20};
-    j9F22.adjCoordinates = {j9F23};
-    j9F23.adjCoordinates = {j9F20, j9F22, j9F37};
-    j9F24.adjCoordinates = {j9F36, j9F25};
-    j9F25.adjCoordinates = {j9F24, j9F26, j9F19};
-    j9F26.adjCoordinates = {j9F27, j9F25, j9F6};
-    j9F27.adjCoordinates = {j9F26, j8F16};
-    j9F28.adjCoordinates = {j9F5, j9F4, j8F2};
-    j9F29.adjCoordinates = {j9F19, j9F18, j8F14};
-    j9F30.adjCoordinates = {j9F13, j9F8, j8F4};
-    j9F31.adjCoordinates = {j9F8, j9F9, j8F6};
-    j9F32.adjCoordinates = {j9F33, j9F15};
-    j9F33.adjCoordinates = {j9F32, j9F16, j9F34};
-    j9F34.adjCoordinates = {j9F33, j9F18};
-    j9F35.adjCoordinates = {j9F19, j9F20};
-    j9F36.adjCoordinates = {j9F37, j9F24, j8F12};
-    j9F37.adjCoordinates = {j9F23, j9F36, j8F9};
+    j9F12.adjCoordinates = {j9F11, j9F13, j8F9};
+    j9F13.adjCoordinates = {j9F12, j9F14, j8F12};
+    j9F14.adjCoordinates = {j9F9, j9F15, j9F13};
+    j9F15.adjCoordinates = {j9F14, j8F16, j9F17};
+    j9F16.adjCoordinates = {j9F15, j8F16};
+    j9F17.adjCoordinates = {j9F6, j9F18};
+    j9F18.adjCoordinates = {j9F17, j9F19};
+    j9F19.adjCoordinates = {j9F18, j9F20};
+    j9F20.adjCoordinates = {j9F19, j9F24, j9F21};
+    j9F21.adjCoordinates = {j9F20, j9F22, j8F6};
+    j9F22.adjCoordinates = {j9F21, j9F23};
+    j9F23.adjCoordinates = {j9F22};
+    j9F24.adjCoordinates = {j9F20, j8F4};
 
-    RoomCoordinate h801 = RoomCoordinate(
-        45.4973307, -73.5785616, '8', 'H', 'SGW',
-        type: "ROOM", roomId: "H801", adjCoordinates: {j8F8, j8F11});
-
-    RoomCoordinate h803 = RoomCoordinate(
-        45.4972776, -73.5786106, '8', 'H', 'SGW',
-        type: "ROOM", roomId: "H803", adjCoordinates: {j8F8, j8F11});
-
-    RoomCoordinate h805 = RoomCoordinate(
-        45.4972277, -73.5786569, '8', 'H', 'SGW',
-        type: "ROOM", roomId: "H805", adjCoordinates: {j8F8, j8F11});
-
-    RoomCoordinate h806 = RoomCoordinate(
-        45.4971761, -73.5788206, '8', 'H', 'SGW',
-        type: "ROOM", roomId: "H806", adjCoordinates: {j8F10, j8F11});
-
-    RoomCoordinate h807 = RoomCoordinate(
-        45.4971802, -73.5787002, '8', 'H', 'SGW',
-        type: "ROOM", roomId: "H807", adjCoordinates: {j8F8, j8F11});
+//    RoomCoordinate h801 = RoomCoordinate(
+//        45.4973307, -73.5785616, '8', 'H', 'SGW',
+//        type: "ROOM", roomId: "H801", adjCoordinates: {j8F8, j8F11});
+//
+//    RoomCoordinate h803 = RoomCoordinate(
+//        45.4972776, -73.5786106, '8', 'H', 'SGW',
+//        type: "ROOM", roomId: "H803", adjCoordinates: {j8F8, j8F11});
+//
+//    RoomCoordinate h805 = RoomCoordinate(
+//        45.4972277, -73.5786569, '8', 'H', 'SGW',
+//        type: "ROOM", roomId: "H805", adjCoordinates: {j8F8, j8F11});
+//
+//    RoomCoordinate h806 = RoomCoordinate(
+//        45.4971761, -73.5788206, '8', 'H', 'SGW',
+//        type: "ROOM", roomId: "H806", adjCoordinates: {j8F10, j8F11});
+//
+//    RoomCoordinate h807 = RoomCoordinate(
+//        45.4971802, -73.5787002, '8', 'H', 'SGW',
+//        type: "ROOM", roomId: "H807", adjCoordinates: {j8F8, j8F11});
 
     RoomCoordinate h811 = RoomCoordinate(
         45.4970862, -73.5787884, '8', 'H', 'SGW',
@@ -550,28 +474,28 @@ class BuildingSingleton {
         45.4973844, -73.5787818, '8', 'H', 'SGW',
         type: "ROOM", roomId: "H860", adjCoordinates: {j8F9});
 
-    RoomCoordinate h861 = RoomCoordinate(
-        45.4973862, -73.5786337, '9', 'H', 'SGW',
-        type: "ROOM", roomId: "H861", adjCoordinates: {j8F7, j8F8});
+//    RoomCoordinate h861 = RoomCoordinate(
+//        45.4973862, -73.5786337, '9', 'H', 'SGW',
+//        type: "ROOM", roomId: "H861", adjCoordinates: {j8F7, j8F8});
 
     RoomCoordinate h862 = RoomCoordinate(
         45.4973047, -73.5787902, '9', 'H', 'SGW',
         type: "ROOM", roomId: "H862", adjCoordinates: {j8F10, j8F12});
 
-    RoomCoordinate h863 = RoomCoordinate(
-        45.4973577, -73.578575, '8', 'H', 'SGW',
-        type: "ROOM", roomId: "H863", adjCoordinates: {j8F7, j8F8});
+//    RoomCoordinate h863 = RoomCoordinate(
+//        45.4973577, -73.578575, '8', 'H', 'SGW',
+//        type: "ROOM", roomId: "H863", adjCoordinates: {j8F7, j8F8});
 
     RoomCoordinate h881 = RoomCoordinate(
         45.4974547, -73.5790906, '8', 'H', 'SGW',
         type: "ROOM", roomId: "H881", adjCoordinates: {j8F4, j8F17});
 
     Floor eightFloor = Floor('8', polygons: floorPolygons['8'], coordinates: {
-      h801,
-      h803,
-      h805,
-      h806,
-      h807,
+//      h801,
+//      h803,
+//      h805,
+//      h806,
+//      h807,
       h811,
       h813,
       h815,
@@ -600,9 +524,9 @@ class BuildingSingleton {
       h854,
       h855,
       h860,
-      h861,
+//      h861,
       h862,
-      h863,
+//      h863,
       h881,
       j8F1,
       j8F2,
@@ -624,15 +548,15 @@ class BuildingSingleton {
     });
     hall.addFloor(eightFloor);
 
-    RoomCoordinate h965 = RoomCoordinate(45.497205, -73.579329, '9', 'H', 'SGW',
-        type: "ROOM", roomId: "H965", adjCoordinates: {j9F1, j9F4});
+    RoomCoordinate h965 = RoomCoordinate(45.497379, -73.578617, '9', 'H', 'SGW',
+        type: "ROOM", roomId: "H965", adjCoordinates: {j9F10, j9F11});
 
-    RoomCoordinate h921 = RoomCoordinate(45.497380, -73.578606, '9', 'H', 'SGW',
-        type: "ROOM", roomId: "H921", adjCoordinates: {j9F20, j9F23});
+    RoomCoordinate h931 = RoomCoordinate(45.497209, -73.579341, '9', 'H', 'SGW',
+        type: "ROOM", roomId: "H931", adjCoordinates: {j9F1, j9F4});
 
     Floor ninthFloor = Floor('9', polygons: floorPolygons['9'], coordinates: {
       h965,
-      h921,
+      h931,
       j9F1,
       j9F2,
       j9F3,
@@ -657,19 +581,6 @@ class BuildingSingleton {
       j9F22,
       j9F23,
       j9F24,
-      j9F25,
-      j9F26,
-      j9F27,
-      j9F28,
-      j9F29,
-      j9F30,
-      j9F31,
-      j9F32,
-      j9F33,
-      j9F34,
-      j9F35,
-      j9F36,
-      j9F37
     });
 
     hall.addFloor(ninthFloor);
