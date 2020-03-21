469f3a9aa4b8b21354d79cb5aad545893554c133
Muhammad Saad Mujtaba <muhanator@hotmail.com>
2020-03-20 22:33:38 -0400

    wrote unit tests for direction class

--- test/provider/direction_notifier_test.dart
+++ test/provider/direction_notifier_test.dart
@@ -1,22 +1,41 @@
+import 'package:concordi_around/data/building_singleton.dart';
+import 'package:concordi_around/model/direction.dart';
 import 'package:concordi_around/service/map_constant.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:concordi_around/provider/direction_notifier.dart';
+import 'package:concordi_around/model/building.dart';
 
 void main() {
-  test('Should be in SGW with no floor plans shown', () {
-    var dn = DirectionNotifier();
+  group("Direction Notifier Unit tests", () {
+    test('Initial values should be correct', () {
+      var dn = DirectionNotifier();
 
-    expect(dn.showDirectionPanel, false);
-    expect(dn.mode, DrivingMode.walking);
+      expect(dn.showDirectionPanel, false);
+      expect(dn.mode, DrivingMode.walking);
+      expect(dn.direction, null);
+      expect(dn.getDirection(), null);
+      expect(dn.directionSteps, List());
+      expect(dn.polylines, []);
+      expect(dn.duration, '0 min');
+      expect(dn.totalDuration, 0);
+      expect(dn.getDuration(), '0 min');
+      expect(dn.totalDistance, 0);
+      expect(dn.apiCallCounter, 0);
+    });
 
-    dn.setShowDirectionPanel(true);
-    expect(dn.showDirectionPanel, true);
+    test('Setter methods should work as expected', () {
+      var dn = DirectionNotifier();
 
-    dn.setDrivingMode(DrivingMode.bicycling);
-    expect(dn.mode, DrivingMode.bicycling);
-    dn.setDrivingMode(DrivingMode.driving);
-    expect(dn.mode, DrivingMode.driving);
-    dn.setDrivingMode(DrivingMode.transit);
-    expect(dn.mode, DrivingMode.transit);
+      dn.setShowDirectionPanel(true);
+      expect(dn.showDirectionPanel, true);
+
+      //Testing the Driving modes
+      dn.setDrivingMode(DrivingMode.bicycling);
+      expect(dn.mode, DrivingMode.bicycling);
+      dn.setDrivingMode(DrivingMode.driving);
+      expect(dn.mode, DrivingMode.driving);
+      dn.setDrivingMode(DrivingMode.transit);
+      expect(dn.mode, DrivingMode.transit);
+    });
   });
 }
