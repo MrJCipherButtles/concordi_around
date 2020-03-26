342656f17e19138bcd6b4cd4ec7becbc67600678
Raffi Jansezian <rjansezian@gmail.com>
2020-03-26 14:43:53 -0400

    Added Google API calls to get place info

--- lib/widget/building_popup.dart
+++ lib/widget/building_popup.dart
@@ -26,7 +26,12 @@ class _BuildingPopupState extends State<BuildingPopup> {
   @override
   Widget build(BuildContext context) {
     MapNotifier mapNotifier = Provider.of<MapNotifier>(context);
-  
+
+    if(currentPlace != mapNotifier.selectedLatlng){
+      currentPlace = mapNotifier.selectedLatlng;
+      _getBuildingDetails(currentPlace);
+    }
+    
     BorderRadiusGeometry radius = BorderRadius.only(
       topLeft: Radius.circular(24.0),
       topRight: Radius.circular(24.0),
@@ -35,6 +40,8 @@ class _BuildingPopupState extends State<BuildingPopup> {
     return Visibility(
       visible: mapNotifier.showInfo,
       child: SlidingUpPanel(
+        minHeight: 80,
+        maxHeight: 450,
         borderRadius: radius,
         backdropEnabled: true,
         panelBuilder: (ScrollController sc) => _scrollingList(sc),
@@ -68,13 +75,13 @@ class _BuildingPopupState extends State<BuildingPopup> {
               bottom: BorderSide(width: 0.5, color: Colors.grey[300])
             )
           ),
-          height: 75,
+          height: 60,
           width: 75,
           child: Stack(
             fit: StackFit.loose,
             children: <Widget>[
               Positioned(
-                top: 9,
+                top: 0,
                 left: 20,
                 child: Text(
                   name,
@@ -85,7 +92,7 @@ class _BuildingPopupState extends State<BuildingPopup> {
                 ),
               ),
               Positioned(
-                top: 37,
+                top: 28,
                 left: 20,
                 child: Text(
                   address,
@@ -105,19 +112,19 @@ class _BuildingPopupState extends State<BuildingPopup> {
               bottom: BorderSide(width: 0.5, color: Colors.grey[300])
             )
           ),
-          height: 60,
+          height: 50,
           width: 60,
           child: Stack(
             fit: StackFit.loose,
             children: <Widget>[
               Positioned(
-                top: 18,
+                top: 12,
                 left: 20,
                 child:
                   Icon(Icons.phone, color: COLOR_CONCORDIA)
               ),
               Positioned(
-                top: 22,
+                top: 16,
                 left: 80,
                 child: Text(
                   phone,
@@ -134,19 +141,19 @@ class _BuildingPopupState extends State<BuildingPopup> {
               bottom: BorderSide(width: 0.5, color: Colors.grey[300])
             )
           ),
-          height: 60,
+          height: 50,
           width: 60,
           child: Stack(
             fit: StackFit.loose,
             children: <Widget>[
               Positioned(
-                top: 18,
+                top: 12,
                 left: 20,
                 child:
                   Icon(Icons.public, color: COLOR_CONCORDIA)
               ),
               Positioned(
-                top: 22,
+                top: 16,
                 left: 80,
                 child: Text(
                   website,
@@ -163,19 +170,19 @@ class _BuildingPopupState extends State<BuildingPopup> {
               bottom: BorderSide(width: 0.5, color: Colors.grey[300])
             )
           ),
-          height: 60,
+          height: 50,
           width: 60,
           child: Stack(
             fit: StackFit.loose,
             children: <Widget>[
               Positioned(
-                top: 18,
+                top: 12,
                 left: 20,
                 child:
                   Icon(Icons.schedule, color: COLOR_CONCORDIA)
               ),
               Positioned(
-                top: 22,
+                top: 16,
                 left: 80,
                 child: Text(
                   openClosed,
@@ -190,9 +197,9 @@ class _BuildingPopupState extends State<BuildingPopup> {
           ),
         ),
         SizedBox(
-          height: 173,
+          height: 168,
           child: ListView.builder(
-            itemCount: pictures.length > 2 ? pictures.length : 0,
+            itemCount: pictures.length,
             scrollDirection: Axis.horizontal,
             itemBuilder: (context, index) => Image(
               image: NetworkImage(pictures[index]),
@@ -202,7 +209,8 @@ class _BuildingPopupState extends State<BuildingPopup> {
         ButtonTheme(
           height: 48,
           child: RaisedButton(
-            onPressed: (){},
+            onPressed: (){
+            },
             color: COLOR_CONCORDIA,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -225,4 +233,60 @@ class _BuildingPopupState extends State<BuildingPopup> {
       ],
     );
   }
+
+  Future<void> _getBuildingDetails(LatLng latLng) async {
+    String baseURL = 'https://maps.googleapis.com/maps/api';
+    double lat = latLng.latitude;
+    double lng = latLng.longitude;
+
+    String geocodingRequest = '$baseURL/geocode/json?latlng=$lat,$lng&result_type=point_of_interest|premise&key=$PLACES_API_KEY';
+    Response geocodingResponse = await Dio().get(geocodingRequest);
+
+    if(geocodingResponse.data['status'] == 'OK'){
+      final geoResult = geocodingResponse.data['results'];
+      String placeId = geoResult[0]['place_id'];
+      String detailsRequest = '$baseURL/place/details/json?place_id=$placeId&key=$PLACES_API_KEY';
+      Response detailsResponse = await Dio().get(detailsRequest);
+      final result = detailsResponse.data['result'];
+
+      name = result['name'];
+
+      if(result['formatted_phone_number'] != null){
+        phone = result['formatted_phone_number'];
+      }else{
+        phone = 'Information Not Available';
+      }
+
+      if(result['website'] != null){
+        website = result['website'];
+      }else{
+        website = 'Information Not Available';
+       }
+
+      if(result['opening_hours'] != null){
+        if(result['opening_hours']['open_now']){
+          openClosed = "Open Now";
+        }else{
+          openClosed = "Closed";
+        }
+      }else{
+        openClosed = 'Information Not Available';
+      }
+
+      String formattedAddress = result['formatted_address'];
+      address = formattedAddress.substring(0, formattedAddress.length - 16);
+
+      dynamic photosResult = result['photos'];
+      pictures.clear();
+
+      if(photosResult != null && result['photos'].length > 2){
+        for(int i = 0; i < result['photos'].length; i++){
+          String photoRef = photosResult[i]['photo_reference'];
+          String picture = '$baseURL/place/photo?maxwidth=500&photoreference=$photoRef&key=$PLACES_API_KEY';
+          pictures.add(picture);
+        }
+      }
+    }
+  }
 }
+
