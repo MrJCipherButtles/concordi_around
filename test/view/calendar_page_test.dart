5a69f329f2454d6f4f5ec28ef5eaf9b6e789640b
ChesterLYU <y-chester@hotmail.com>
2020-04-18 14:33:37 -0400

    - added test for the calendar_page.dart

---
+++ test/view/calendar_page_test.dart
@@ -0,0 +1,35 @@
+import 'package:concordi_around/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+void main() {
+  final TestWidgetsFlutterBinding binding = TestWidgetsFlutterBinding.ensureInitialized();
+  testWidgets('Testing the calendar page widget', (WidgetTester tester) async {
+    await binding.setSurfaceSize(Size(640, 640));
+    //building the application
+    await tester.pumpWidget(MyApp());
+    await tester.pumpAndSettle();
+
+    expect(find.byIcon(Icons.menu), findsOneWidget);
+    expect(find.byIcon(Icons.directions), findsOneWidget);
+    expect(find.byIcon(Icons.my_location), findsOneWidget);
+    expect(find.text('Search'), findsOneWidget);
+
+    //open sidebar to reach the calendar page
+    await tester.tap(find.byIcon(Icons.menu));
+    await tester.pumpAndSettle();
+
+    expect(find.text('My Calendar'), findsOneWidget);
+    expect(find.text('Settings'), findsOneWidget);
+    expect(find.text('Contact Us'), findsOneWidget);
+    expect(find.text('About'), findsOneWidget);
+
+    await tester.tap(find.text('My Calendar'));
+    await tester.pumpAndSettle();
+
+    expect(find.text('My Calendar'), findsOneWidget);
+    expect(find.byIcon(Icons.arrow_forward), findsOneWidget);
+    expect(find.byType(TableCalendar), findsOneWidget);
+  });
+}
