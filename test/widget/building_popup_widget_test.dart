783f6aceba950bbd6431426f54b28cfd4178c35f
ChesterLY <y-chester@hotmail.com>
2020-04-18 19:05:58 -0400

    Test/map and search test (#152)
    * - snackbar was added to the goto_page that triggers when an invalid (or no) destination was set
    
    * - added test for setPopupInfoVisibility(bool visibility) and
    setPlaceLatLng(LatLng latLng)
    
    * - added direction button test for map.dart
    - added 'dragging' motion test
    
    * - removed problematic test that caused build failure
    
    * testing direction panel
    
    * - added some tests
    
    * - added widget test for building_popup.dart
    
    * - fixed test error
    
    * - added unit test for marker_helper.dart
    
    * - added additional test for map.dart and modified the app building to remove warning
    
    * - remove the TODO for testing Futures as requested
    
    * - coverage dropped because of an test removed by accident, fixed
    
    * - additional tests for marker_helper.dart
    
    * - added test for the calendar_page.dart
    
    * - removed 1 expect() from calendar_test.dart that is in conflict with a hotfix
    
    Co-authored-by: Muhammad Saad Mujtaba <muhanator@hotmail.com>

---
+++ test/widget/building_popup_widget_test.dart
@@ -0,0 +1,34 @@
+import 'package:concordi_around/provider/map_notifier.dart';
+import 'package:concordi_around/widget/building_popup.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  Widget makeTestableWidget({Widget child}) {
+    return MaterialApp(
+      home: Scaffold(
+        body: child,
+      ),
+    );
+  }
+
+  testWidgets('Testing the Building Popup widget', (WidgetTester tester) async {
+    var notifier = MapNotifier();
+    notifier.showInfo = true;
+
+    await tester.pumpWidget(makeTestableWidget(
+      child: ChangeNotifierProvider(
+        create: (_) => notifier,
+        child: BuildingPopup(),
+      ),
+    ));
+
+    await tester.pumpAndSettle();
+
+    expect(find.byIcon(Icons.close), findsOneWidget);
+    expect(find.byType(ListView), findsWidgets);
+    expect(find.byType(RaisedButton), findsOneWidget);
+
+  });
+}
